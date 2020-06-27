48bd9a841d277fff6cf51c4d132d311143f81c07
Anna Krieger <annakrieger@web.de>
2020-06-27 15:24:22 +0200

    Bilder anstatt Piktogramme

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,15 +2,17 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 
 import "./Card.css";
+//import symbol from "../../pictures/Kartoffel.jpg"; // relative path to image 
 
 class Card extends PureComponent {
     render() {
         const { style, symbol } = this.props;
+        console.log(symbol)
         return (
             <div className="card-wrapper">
                 <button className="card" style={style} onClick={() => this.props.onClick()}>
                     <div className="front" />
-                    <div className="back">{symbol}</div>
+                    <div className="back"><img src={window.location.origin + symbol} alt="as" width="100" height="100"></img></div>
                 </button>
             </div>
         );
