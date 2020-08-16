e5b88e898739899659a69d87e1bfc678f0953ca1
Gisela Grabe <katrin@grabe-online.de>
2020-08-16 18:48:41 +0200

    other titels for the different levels

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,11 +12,11 @@ const height ="20px";
 const Home = () => (
     <div className="home">
         <h1 className="header">Globales Memory<img src={logo}></img></h1>
-        <div className = "description-container"><p>Wie viel weißt du eigentlich über die Lebensmittel, die du jeden Tag vor deiner Nase hast? Kannst du erraten, an was für einer Pflanze Früchte und Gemüse wachsen? Versuche es mal in dem Memory-Spiel.</p><p>Für uns ist es ganz normal jeden Tag Kartoffeln, Nudeln oder Bananen zu essen. Aber diese Lebensmittel gibt es noch gar nicht so lange in Deutschland. Alle diese Lebensmittel haben einen langen Weg hinter sich. Wo kommen sie ursprünglich her und wie kamen sie nach Deutschland? All’ das kannst du erfahren, wenn du das Memory erfolgreich geschafft hast!</p><p>1: "Kleines Memory" mit 5 verschiedenen Gemüse- und Obstsorten (Obst - Pflanze - Produkt) </p><p>2: “Großes Memory” mit 10 verschiedenen Gemüse- und Obstsorten (Obst - Pflanze - Produkt)</p><p>3: Ländermemory (Obst - Pflanze - Ursprungsland)</p></div>
+        <div className = "description-container"><p>Wie viel weißt du eigentlich über die Lebensmittel, die du jeden Tag vor deiner Nase hast? Kannst du erraten, an was für einer Pflanze Früchte und Gemüse wachsen? Versuche es mal in dem Memory-Spiel.</p><p>Für uns ist es ganz normal jeden Tag Kartoffeln, Nudeln oder Bananen zu essen. Aber diese Lebensmittel gibt es noch gar nicht so lange in Deutschland. Alle diese Lebensmittel haben einen langen Weg hinter sich. Wo kommen sie ursprünglich her und wie kamen sie nach Deutschland? All’ das kannst du erfahren, wenn du das Memory erfolgreich geschafft hast!</p><p>1: "Kleines Memory" mit 5 verschiedenen Gemüse- und Obstsorten (Obst - Pflanze - Produkt) </p><p>2: “Großes Memory” mit 10 verschiedenen Gemüse- und Obstsorten (Obst - Pflanze - Produkt)</p><p>3: "Ländermemory" (Obst - Pflanze - Ursprungsland)</p></div>
         <div className="difficulty-container">
-            <div className="homeLinkClass"> <Link to="/game/easy" ><img src={memory} height ={height}></img>Spiel mit 15 Karten</Link></div>
-            <div className="homeLinkClass">  <Link to="/game/hard"><img src={doubleMemory} height ={height}></img>Spiel mit allen 30 Karten</Link></div>
-            <div className="homeLinkClass"> <Link to="/game/triples"><img src={gurke} height ={height}></img>Spiel mit den Früchten und ihren Ursprungsländern</Link></div>
+            <div className="homeLinkClass"> <Link to="/game/easy" ><img src={memory} height ={height}></img>Kleines Memory</Link></div>
+            <div className="homeLinkClass">  <Link to="/game/hard"><img src={doubleMemory} height ={height}></img>Großes Memory</Link></div>
+            <div className="homeLinkClass"> <Link to="/game/triples"><img src={gurke} height ={height}></img>Ländermemory</Link></div>
         </div>
     </div>
 );
