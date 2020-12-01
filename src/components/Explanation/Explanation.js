e0096fad59ecfc1cba4bcb8549f2972544a3322c
Maddin <martin.wuerflein@gmx.de>
2020-12-01 22:19:47 +0100

    hover effect on result page

--- src/components/Explanation/Explanation.js
+++ src/components/Explanation/Explanation.js
@@ -61,7 +61,7 @@ setOverlay(toDisplay, explanation, images) {
                     {   
                     cards.map(function (d, idx) {
 
-                        return (<li key={idx} onClick={() => reference.setOverlay(true, finished?<div class="centerText">d.text1</div>:<div class="centerText"> d.text</div>, d.image_paths)}>{d.name}
+                        return (<li key={idx} onClick={() => reference.setOverlay(true, finished?d.text1:d.text, d.image_paths)}><div class="centerText">{d.name}</div>
                             <img src={window.location.origin + d.image_paths[0]} alt={d.name} width="100" height="100" ></img>
                             <img src={window.location.origin + d.image_paths[1]} alt={d.name} width="100" height="100" ></img>
                             <img src={window.location.origin + d.image_paths[2]} alt={d.name} width="100" height="100" ></img>
