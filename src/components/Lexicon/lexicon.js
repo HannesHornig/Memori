f6b358c7f3bfe11e2459b0075a94db8358b591ca
Gisela Grabe <katrin@grabe-online.de>
2021-03-03 20:55:41 +0100

    lexicon

---
+++ src/components/Lexicon/lexicon.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../../pictures/Grenzgaenger_Logo.PNG";
+import { Link } from "react-router-dom";
+
+const Lexicon = () => (
+<div className="lexicon">
+        <div>
+
+        <h2>Lexikon</h2>
+<p>Auf dieser Seite könnt ihr schwierige Wörter nachschlagen</p>
+
+<h3>Wort 1</h3>
+<p>Erklärund Wort 1</p>
+<h3>Wort 2</h3>
+<p>Erklärung Wort 2</p>
+<h3>Wort 3</h3>
+<p>Erklärund Wort 3</p>
+<h3>Wort 4</h3>
+<p>Erklärung Wort 4</p>
+        </div>
+    </div>
+    
+);
+
+export default Lexicon;
