05ca10ab1489c6cc88ebf0309e0a124352c61a18
Adam Rinder <rindeada@b-tu.de>
2021-05-29 13:36:48 +0200

    re-added sound from master

--- src/components/Explanation/Explanation.js
+++ src/components/Explanation/Explanation.js
@@ -182,7 +182,7 @@ testBg(textPage, stage) {
                     {   
                     cards.map(function (d, idx) {
 
-                        return (<li class="cardItem" key={idx} onClick={() =>  reference.setOverlay(true, status?[{title:'Wo komme ich her',text:parse(d.texts[1])},{title:'Mein Weg in die Welt hinaus',text:parse(d.texts[2])}]:[ {title:'', text:parse(d.texts[0]) }], d.image_paths[status?0:1],status?d.sounds:null)}><div class="centerText">{d.name}</div>
+                        return (<li class="cardItem" key={idx} onClick={() =>  reference.setOverlay(true, status?[{title:'Wo komme ich her',text:parse(d.texts[1])},{title:'Mein Weg in die Welt hinaus',text:parse(d.texts[2])}]:[ {title:'', text:parse(d.texts[0]) }], d.image_paths[status?0:1],status?[d.sounds[1],d.sounds[2]]:[d.sounds[0]])}><div class="centerText">{d.name}</div>
                                 
                                 <img src={window.location.origin + d.image_paths[0]} alt={d.name} />
                                 {status==0&&<img src={window.location.origin + d.image_paths[1]} alt={d.name} />}
