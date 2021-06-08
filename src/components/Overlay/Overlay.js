750996d0fa4620d760e8c525d1f2b2121fc8c4eb
Maddin <martin.wuerflein@gmx.de>
2021-06-08 23:54:29 +0200

    sounds stopping in map layout - some cleaning

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import "./Overlay.css";
-import UIfx from 'uifx';
 
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 
 let sound;
 
@@ -80,8 +79,6 @@ class Overlay extends React.Component {
             playing: false
         });
     }
-
-
 
     render() {
         return (
